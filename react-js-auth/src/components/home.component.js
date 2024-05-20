0e6fa447695d5f0b516620424ac73c339e438cd5
Gilberto <gilbertodsbatista@gmail>
2024-05-20 20:03:25 -0300

    Adicionando projto front-end em react, para consumir a API em node.js

---
+++ react-js-auth/src/components/home.component.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+import UserService from "../services/user.service";
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: ""
+    };
+  }
+
+  componentDidMount() {
+    UserService.getPublicContent().then(
+      response => {
+        this.setState({
+          content: response.data
+        });
+      },
+      error => {
+        this.setState({
+          content:
+            (error.response && error.response.data) ||
+            error.message ||
+            error.toString()
+        });
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div class="p-5 mb-4 bg-light rounded-3">
+          <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">{this.state.content}</h1>
+            <p class="col-md-8 fs-4">Seja bem-vindo. Agradecemos pela preferência.</p>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
